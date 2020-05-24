f9b99a56f3f54fd61b7d8b1b96a94571d5a297b4
liam roberts <liamroberts77@googlemail.comgit config --global user.email liamroberts77@googlemail.com>
2020-05-24 21:00:28 +0100

    passed first test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
-require 'docking_station'
+require  './lib/docking_station.rb'
 
 describe DockingStation do
+
+    it "responds to release_bike" do
+        expect(subject).to respond_to :release_bike
+    end
     
 end
